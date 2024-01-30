d26d8c49fda1ac5d9db7ddb0391ac132e059b83e
sjamillah <j.ssozi@alustudent.com>
2024-01-30 07:01:33 -0800

    updated state

--- models/state.py
+++ models/state.py
@@ -14,8 +14,8 @@ class State(BaseModel, Base):
     name = Column(String(128), nullable=False)
 
     if os.getenv("HBNB_TYPE_STORAGE") == "db":
-        cities = relationship('City', backref='state', 
-                                cascade='all, delete-orphan')
+        cities = relationship('City', backref='state',
+                cascade='all, delete-orphan')
 
     else:
         @property
