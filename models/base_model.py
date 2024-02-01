f8ca0b5983b71f0680f0f32dbe7b1ebf3dc8b84a
sjamillah <j.ssozi@alustudent.com>
2024-02-01 06:15:32 -0800

    updated the base model

--- models/base_model.py
+++ models/base_model.py
@@ -3,7 +3,7 @@
 import uuid
 from datetime import datetime
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, String, DateTime, Integer
+from sqlalchemy import Column, String, DateTime
 
 
 Base = declarative_base()
