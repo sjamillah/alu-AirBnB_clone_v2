e87473bc0b1c40b8e9a467b9b3ac6b631f4da13b
sjamillah <j.ssozi@alustudent.com>
2024-02-01 08:20:56 -0800

    updated the main delete file

--- main_delete.py
+++ main_delete.py
@@ -8,12 +8,7 @@ fs = FileStorage()
 
 # All States
 all_states = fs.all(State)
-if all_states is not None:
-    print("All States: {}".format(len(all_states.keys())))
-    for state_key in all_states.keys():
-        print(all_states[state_key])
-else:
-    print("Error: fs.all(State) returned None")
+print("All States: {}".format(len(all_states.keys())))
 
 # Create a new State
 new_state = State()
@@ -24,12 +19,9 @@ print("New State: {}".format(new_state))
 
 # All States
 all_states = fs.all(State)
-if all_states is not None:
-    print("All States: {}".format(len(all_states.keys())))
-    for state_key in all_states.keys():
-        print(all_states[state_key])
-else:
-    print("Error: fs.all(State) returned None")
+print("All States: {}".format(len(all_states.keys())))
+for state_key in all_states.keys():
+    print(all_states[state_key])
 
 # Create another State
 another_state = State()
@@ -40,21 +32,18 @@ print("Another State: {}".format(another_state))
 
 # All States
 all_states = fs.all(State)
-if all_states is not None:
-    print("All States: {}".format(len(all_states.keys())))
-    for state_key in all_states.keys():
-        print(all_states[state_key])
-else:
-    print("Error: fs.all(State) returned None")
+print("All States: {}".format(len(all_states.keys())))
+for state_key in all_states.keys():
+    print(all_states[state_key])
 
 # Delete the new State
 fs.delete(new_state)
 
-# All States
+# Update all_states after deletion
 all_states = fs.all(State)
-if all_states is not None:
-    print("All States: {}".format(len(all_states.keys())))
-    for state_key in all_states.keys():
-        print(all_states[state_key])
-else:
-    print("Error: fs.all(State) returned None")
+
+# All States
+print("All States: {}".format(len(all_states.keys())))
+for state_key in all_states.keys():
+    print(all_states[state_key])
+
