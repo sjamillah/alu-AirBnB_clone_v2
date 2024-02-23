a8ff508c230617bff9a6bf2e931662de8f6462d8
sjamillah <j.ssozi@alustudent.com>
2024-02-23 20:39:27 +0000

    errors checking

--- models/__init__.py
+++ models/__init__.py
@@ -6,10 +6,12 @@ storage_type = os.getenv('HBNB_TYPE_STORAGE')
 
 if storage_type == "db":
     from models.engine.db_storage import DBStorage
+    
     storage = DBStorage()
+    storage.reload()
 
 else:
     from models.engine.file_storage import FileStorage
+    
     storage = FileStorage()
-
-storage.reload()
+    storage.reload()
